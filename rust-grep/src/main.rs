2d300797f5e32bdfc6a53524f8497ad4f64b0e76
Alfred Kristal Ern <alfred.kristal@gmail.com>
2022-11-14 13:05:59 +0100

    Split logic into lib.rs

--- rust-grep/src/main.rs
+++ rust-grep/src/main.rs
@@ -1,6 +1,6 @@
 use std::env;
-use std::fs;
-use std::error::Error;
+
+use rust_grep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,34 +9,8 @@ fn main() {
         std::process::exit(1);
     });
 
-    if let Err(e) = run(config) {
+    if let Err(e) = rust_grep::run(config) {
         println!("Application error: {}", e);
         std::process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-    
-        Ok(Config { query, filename })
     }
 }
