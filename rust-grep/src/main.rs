3b87fc2ca22f5ca098dc26e0d77ff903dc9bd781
Alfred Kristal Ern <alfred.kristal@gmail.com>
2022-11-14 12:53:38 +0100

    Refactored and add error handling

--- rust-grep/src/main.rs
+++ rust-grep/src/main.rs
@@ -3,8 +3,15 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        std::process::exit(1);
+    });
 
+    run(config);
+}
+
+fn run(config: Config) {
     let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
 
@@ -16,9 +23,15 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
 
-    Config { query, filename }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+    
+        Ok(Config { query, filename })
+    }
 }
