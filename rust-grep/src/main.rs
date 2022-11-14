a7d9cab198faa57f7f73f9b30528f93660d3040e
Alfred Kristal Ern <alfred.kristal@gmail.com>
2022-11-14 13:22:14 +0100

    Remove debug prints

--- rust-grep/src/main.rs
+++ rust-grep/src/main.rs
@@ -5,12 +5,10 @@ use rust_grep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
         std::process::exit(1);
     });
 
     if let Err(e) = rust_grep::run(config) {
-        println!("Application error: {}", e);
         std::process::exit(1);
     }
 }
